a6b9d436d32568139603224b595bd78d61ae7de6
geoffreychiou <geoffrey.chiou@gmail.com>
2017-01-04 18:33:05 -0600

    Fixed bug where numbers would go over minimum value

--- godel_scan_tools/include/godel_scan_tools/edge_refinement.h
+++ godel_scan_tools/include/godel_scan_tools/edge_refinement.h
@@ -492,7 +492,7 @@ public:
 
     if (event.getKeySym() == "Right" && event.keyDown())
     {
-      if (CURRENT_POSE_INDEX >= 0 && CURRENT_POSE_INDEX < NUM_POSES)
+      if (CURRENT_POSE_INDEX > 0 && CURRENT_POSE_INDEX < NUM_POSES)
       {
         CURRENT_POSE_INDEX++;
       }
@@ -501,7 +501,7 @@ public:
 
     if (event.getKeySym() == "Left" && event.keyDown())
     {
-      if (CURRENT_POSE_INDEX >= 0 && CURRENT_POSE_INDEX < NUM_POSES)
+      if (CURRENT_POSE_INDEX > 0 && CURRENT_POSE_INDEX < NUM_POSES)
       {
         CURRENT_POSE_INDEX--;
       }
